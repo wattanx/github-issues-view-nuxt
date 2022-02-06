8407b25df5ab7712f9e5be157c745a13632df392
wattanx <wattan.dev@gmail.com>
2022-02-06 23:10:31 +0900

    fix: useAsyncがうまく行かないので戻す

--- pages/issues/index.vue
+++ pages/issues/index.vue
@@ -1,44 +1,3 @@
-<script setup lang="ts">
-import {
-  defineComponent,
-  reactive,
-  ref,
-  useAsync,
-  useContext,
-  useRoute,
-} from '@nuxtjs/composition-api';
-import { CBox, CFlex } from '@chakra-ui/vue';
-import { getIssues } from '@/framework';
-import ContentWrapper from '@/components/ContentWrapper.vue';
-import IssuesListView from '@/components/IssuesListView.vue';
-import Pager from '@/components/Pager/Pager.vue';
-import { AxiosClient } from '~/framework';
-import { IssueType } from '~/types';
-
-const { query } = useContext();
-
-const pageNumber = query.value.page ?? '1';
-const perPage = query.value.per_page ?? '10';
-console.log(pageNumber);
-
-const issues = ref<IssueType[]>([]);
-const prev = ref<number>(0);
-const next = ref<number>(0);
-const current = ref<number>(0);
-const last = ref<number>(0);
-useAsync(async () => {
-  const res = await getIssues(
-    new AxiosClient(),
-    pageNumber as string,
-    perPage as string,
-  );
-  issues.value = res.issues;
-  prev.value = res.prevPage;
-  next.value = res.nextPage;
-  current.value = res.currentPage;
-  last.value = res.lastPage;
-}, pageNumber as string);
-</script>
 <template>
   <ContentWrapper>
     <CBox border-width="1px" border-color="#e2e2e2" border-radius="6px">
@@ -55,3 +14,41 @@ useAsync(async () => {
     </CFlex>
   </ContentWrapper>
 </template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+import { CBox, CFlex } from '@chakra-ui/vue';
+import { getIssues } from '@/framework';
+import ContentWrapper from '@/components/ContentWrapper.vue';
+import IssuesListView from '@/components/IssuesListView.vue';
+import Pager from '@/components/Pager/Pager.vue';
+import { AxiosClient } from '~/framework';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    ContentWrapper,
+    CBox,
+    CFlex,
+    IssuesListView,
+    Pager,
+  },
+  async asyncData({ query }) {
+    const pageNumber = query.page ?? '1';
+    const perPage = query.per_page ?? '10';
+    const res = await getIssues(
+      new AxiosClient(),
+      pageNumber as string,
+      perPage as string,
+    );
+    return {
+      issues: res.issues,
+      prev: res.prevPage,
+      next: res.nextPage,
+      current: res.currentPage,
+      last: res.lastPage,
+    };
+  },
+  watchQuery: ['page'],
+});
+</script>
