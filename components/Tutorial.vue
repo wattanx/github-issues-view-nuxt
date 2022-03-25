e8c3b102cf9a4a663f1cf2e9a4f1d37dd14c8b1e
wattanx <wattan.dev@gmail.com>
2022-03-25 17:14:38 +0900

    reduce bundle

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,15 +1,7 @@
 <!-- Please remove this file from your project -->
 <template>
   <div
-    class="
-      relative
-      flex
-      items-top
-      justify-center
-      min-h-screen
-      bg-gray-100
-      sm:items-center sm:pt-0
-    "
+    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
   >
     <link
       href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css"
@@ -62,16 +54,7 @@
         <h2 class="text-2xl leading-7 font-semibold">
           Welcome to your Nuxt Application
         </h2>
-        <p class="mt-3 text-gray-600">
-          We recommend you take a look at the
-          <a
-            href="https://nuxtjs.org"
-            target="_blank"
-            class="text-green-500 hover:underline"
-            >Nuxt documentation</a
-          >, whether you are new or have previous experience with the
-          framework.<br />
-        </p>
+
         <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
           To get started, remove
           <code class="bg-gray-100 text-sm p-1 rounded border"
