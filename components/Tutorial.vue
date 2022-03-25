69e55047b2503051fa24b874d379279e03d0d8d0
wattanx <wattan.dev@gmail.com>
2022-03-25 17:20:00 +0900

    reduce bundle

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,15 +1,7 @@
 <!-- Please remove this file from your project -->
 <template>
   <div
-    class="
-      relative
-      flex
-      items-top
-      justify-center
-      min-h-screen
-      bg-gray-100
-      sm:items-center sm:pt-0
-    "
+    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
   >
     <link
       href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css"
@@ -59,9 +51,6 @@
         </svg>
       </a>
       <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
-        <h2 class="text-2xl leading-7 font-semibold">
-          Welcome to your Nuxt Application
-        </h2>
         <p class="mt-3 text-gray-600">
           We recommend you take a look at the
           <a
