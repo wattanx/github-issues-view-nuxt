2e066e089a5ca8fc335e40f74aaa089ef60a7005
wattanx <wattan.dev@gmail.com>
2022-04-16 16:07:35 +0900

    fix: bundle

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,9 @@
 <script setup lang="ts">
 import { CBox } from '@chakra-ui/vue';
 import Container from '@/components/Container.vue';
-import CTA from '@/components/CTA.vue';
-import Hero from '@/components/Hero.vue';
 </script>
 <template>
   <Container height="100vh">
-    <CBox as="section" pt="6rem" :pb="{ base: '0', md: '5rem' }">
-      <CBox text-align="center">
-        <Hero />
-      </CBox>
-    </CBox>
-    <CTA />
+    <CBox as="section" pt="6rem" :pb="{ base: '0', md: '5rem' }"> </CBox>
   </Container>
 </template>
