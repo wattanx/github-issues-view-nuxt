8ec6ac0b0d67580e9c41ac2c98d35114c964e809
wattanx <wattan.dev@gmail.com>
2022-04-16 16:11:19 +0900

    Revert "fix: bundle"
    This reverts commit 2e066e089a5ca8fc335e40f74aaa089ef60a7005.

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 import { CBox } from '@chakra-ui/vue';
 import Container from '@/components/Container.vue';
+import CTA from '@/components/CTA.vue';
+import Hero from '@/components/Hero.vue';
 </script>
 <template>
   <Container height="100vh">
-    <CBox as="section" pt="6rem" :pb="{ base: '0', md: '5rem' }"> </CBox>
+    <CBox as="section" pt="6rem" :pb="{ base: '0', md: '5rem' }">
+      <CBox text-align="center">
+        <Hero />
+      </CBox>
+    </CBox>
+    <CTA />
   </Container>
 </template>
