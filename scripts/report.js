e658a96ee4895d3480529f10b0cfad1c67f89e4e
wattanx <wattan.dev@gmail.com>
2022-04-17 14:25:41 +0900

    Update report.js

--- scripts/report.js
+++ scripts/report.js
@@ -3,41 +3,20 @@
 const fs = require('fs');
 const path = require('path');
 const zlib = require('zlib');
-const filesize = require('filesize');
 const mkdirp = require('mkdirp');
 
 const memoryCache = {};
 
-function getScriptSizes(scriptPaths, buildOutputDir) {
-  const res = scriptPaths.reduce(
-    (acc, scriptPath) => {
-      const [rawSize, gzipSize] = getScriptSize(
-        path.join(buildOutputDir, 'dist/client', scriptPath),
-      );
-      acc.raw += rawSize;
-      acc.gzip += gzipSize;
-      return acc;
-    },
-    { raw: 0, gzip: 0 },
-  );
-  return res;
-}
-
 function getScriptSize(scriptPath) {
-  const encoding = 'utf8';
-
-  let rawSize, gzipSize;
   if (Object.keys(memoryCache).includes(scriptPath)) {
-    rawSize = memoryCache[scriptPath][0];
-    gzipSize = memoryCache[scriptPath][1];
-  } else {
-    const bytes = fs.readFileSync(scriptPath, encoding);
-    rawSize = Buffer.byteLength(bytes, encoding);
-    gzipSize = zlib.gzipSync(bytes).byteLength;
-    memoryCache[scriptPath] = [rawSize, gzipSize];
+    return memoryCache[scriptPath];
   }
 
-  return [rawSize, gzipSize];
+  const bytes = fs.readFileSync(scriptPath, 'utf8');
+  const gzipSize = zlib.gzipSync(bytes).byteLength;
+  memoryCache[scriptPath] = gzipSize;
+
+  return gzipSize;
 }
 
 const {
@@ -64,14 +43,18 @@ try {
   process.exit(1);
 }
 
-const allPageSizes = Object.values(statsFile.namedChunkGroups).reduce(
-  (acc, scriptPath, i) => {
-    const pagePath = Object.keys(statsFile.namedChunkGroups)[i];
-    const scriptSizes = getScriptSizes(scriptPath.assets, buildOutputDir);
-
-    return [...acc, { path: pagePath, size: scriptSizes.gzip }];
+const allPageSizes = Object.entries(statsFile.namedChunkGroups).map(
+  ([key, value]) => {
+    const size = value.assets
+      .map((scriptPath) => {
+        const gzipSize = getScriptSize(
+          path.join(buildOutputDir, 'dist/client', scriptPath),
+        );
+        return gzipSize;
+      })
+      .reduce((s, b) => s + b, 0);
+    return { path: key, size };
   },
-  [],
 );
 
 const rawData = JSON.stringify(allPageSizes);
