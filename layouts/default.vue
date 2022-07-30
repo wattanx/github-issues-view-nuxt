ec956ff50d97fe03702a5dedbe470289c882278f
wattanx <wattan.dev@gmail.com>
2022-07-30 22:12:54 +0900

    fix: chakra uiを削除

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,21 +7,12 @@
   </div>
 </template>
 <script>
-import {
-  CThemeProvider,
-  CColorModeProvider,
-  CReset,
-  CBox,
-} from '@chakra-ui/vue';
 import SiteHeader from '@/components/SiteHeader.vue';
 import Box from '~/components/Box.vue';
 
 export default {
   name: 'App',
   components: {
-    CThemeProvider,
-    CColorModeProvider,
-    CReset,
     Box,
     SiteHeader,
   },
